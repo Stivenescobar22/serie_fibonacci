4cbaaaad51e1762c162746c196254ab58febbc0b
Stivenescobar22 <estibenes@gmail.com>
2019-09-15 18:56:02 -0500

    programa que realiza la serie fibonacci de un numjero asignado

---
+++ serie_Fibonnacci/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace serie_Fibonnacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.lsbsucesion = new System.Windows.Forms.ListBox();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.lblmensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(56, 21);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(190, 20);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Sucesion de Fibonacci";
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero.Location = new System.Drawing.Point(32, 68);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(136, 15);
+            this.lblnumero.TabIndex = 1;
+            this.lblnumero.Text = "Ingrese un numero: ";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero.Location = new System.Drawing.Point(35, 91);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(133, 22);
+            this.txtnumero.TabIndex = 2;
+            this.txtnumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnumero_KeyPress);
+            // 
+            // lsbsucesion
+            // 
+            this.lsbsucesion.FormattingEnabled = true;
+            this.lsbsucesion.Location = new System.Drawing.Point(35, 175);
+            this.lsbsucesion.Name = "lsbsucesion";
+            this.lsbsucesion.Size = new System.Drawing.Size(187, 147);
+            this.lsbsucesion.TabIndex = 3;
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(184, 91);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnmostrar.TabIndex = 4;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(273, 90);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(75, 23);
+            this.btnborrar.TabIndex = 5;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(273, 318);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 6;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // lblmensaje
+            // 
+            this.lblmensaje.AutoSize = true;
+            this.lblmensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmensaje.Location = new System.Drawing.Point(35, 142);
+            this.lblmensaje.Name = "lblmensaje";
+            this.lblmensaje.Size = new System.Drawing.Size(62, 15);
+            this.lblmensaje.TabIndex = 7;
+            this.lblmensaje.Text = "mensaje";
+            this.lblmensaje.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 353);
+            this.Controls.Add(this.lblmensaje);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.lsbsucesion);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.lblnumero);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.ListBox lsbsucesion;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Label lblmensaje;
+    }
+}
+
---
+++ serie_Fibonnacci/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serie_Fibonnacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            string numero = txtnumero.Text;
+
+            lsbsucesion.Items.Clear();
+
+            int num = int.Parse(numero);
+            int numero1 = 0;
+            int numero2 = 1;
+            int sumnumero3 = 0;
+
+
+            lsbsucesion.Items.Add(numero1);
+            lsbsucesion.Items.Add(numero2);
+
+            lblmensaje.Text = " sucesion fibonaccci - Limite " + num;
+            lblmensaje.Visible = true;
+
+            
+
+            for (int i = 3; i <= num; i++){
+
+                if (i <= num){
+
+                    sumnumero3 = numero1 + numero2;
+                    lsbsucesion.Items.Add(sumnumero3);
+                    numero1 = numero2;
+                    numero2 = sumnumero3;
+
+                }
+
+                txtnumero.Text = "";
+                txtnumero.Focus();
+            }
+
+           
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            lsbsucesion.Items.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtnumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar)){
+
+                e.Handled = false;
+
+            }else if (char.IsControl(e.KeyChar)){
+
+                e.Handled = false;
+
+            }else if (char.IsSeparator(e.KeyChar)){
+
+                e.Handled = false;
+            }else{
+
+                MessageBox.Show("Solo se admiten Numeros"
+                    , "Mensaje de Advertencia"
+                    ,MessageBoxButtons.OK
+                    ,MessageBoxIcon.Error);
+                e.Handled = true;
+                return;
+
+            }
+        }
+    }
+}
